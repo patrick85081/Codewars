f66c008e39c00b416d04ccdf75d49c643c5f9f1a
Patrick <patrick85081@gmail.com>
2018-09-06 22:32:10 +0800

    使用 Regex重構 DubstepSongDecoder

--- Codewars/Dubstep.cs
+++ Codewars/Dubstep.cs
@@ -1,16 +1,21 @@
-﻿namespace Codewars
+﻿using System.Text.RegularExpressions;
+
+namespace Codewars
 {
     public class Dubstep
     {
-        private const string OneSpace= " ";
-        private const string TwoSpace = "  ";
+        //private const string OneSpace= " ";
+        //private const string TwoSpace = "  ";
 
         public string SongDecoder(string song)
         {
-            return song.Replace("WUB", OneSpace)
-                .Replace(TwoSpace, OneSpace)
-                .Replace(TwoSpace, OneSpace)
+            return Regex.Replace(song, @"(WUB)+", " ")
                 .Trim();
+
+            //return song.Replace("WUB", OneSpace)
+            //    .Replace(TwoSpace, OneSpace)
+            //    .Replace(TwoSpace, OneSpace)
+            //    .Trim();
         }
     }
 }
